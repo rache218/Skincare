29c123650352e2da909283f5dba87beba256f386
rachel <opakunlerachel@gmail.com>
2026-01-03 18:17:07 +0100

    first commit

---
+++ skincare.js
@@ -0,0 +1,6 @@
+// HAMBURGER MENU
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
